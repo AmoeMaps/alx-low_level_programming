d1951418801aa56f239ef57ab3c994443459843d
AmoeMaps <amoemaps@gmail.com>
2023-06-21 04:37:41 -0700

    10-add.c

--- 0x02-functions_nested_loops/8-main.c
+++ 0x02-functions_nested_loops/8-main.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * main - check the code for Holberton School students.
+ * * main - check the code for Holberton School students.
  *
  * Return: Always 0.
+ *
  */
 int main(void)
 {
