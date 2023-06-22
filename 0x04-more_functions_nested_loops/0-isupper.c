d30cb54a2ac43ca32c4cedc4d5c3a8423ebfdc07
AmoeMaps <amoemaps@gmail.com>
2023-06-22 05:59:06 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _supper - check if a letter is upper
+ * _isupper - check if a letter is upper
  * @x: The number to be checked
  * Return: 1 for upper letter or 0 for any else
  */
 
-int _supper(int x)
+int _isupper(int x)
 
 {
 	if (x >= 65 && x <= 98)
